d8477e6ba3a41b2e9b74eddff35a4eff74415ce3
MacB3Toon <126741808+MacB3Toon@users.noreply.github.com>
2023-03-08 15:00:18 -0600

    array assignment

--- 2-bouncingballsdemo/index.md
+++
@@ -1,7 +0,0 @@
-# CS 30 Demos - sem 2 - 2023
-
-## demos
-- [bouncing balls] (2-bouncingballsdemo)
-- [Cubic Dissaray] (3-cubicdiss)
-
-## Projects
---
+++ array-assignment/sketch.js
@@ -0,0 +1,15 @@
+// Arrays and object notation assignment
+// Macayla Buckmaster
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
