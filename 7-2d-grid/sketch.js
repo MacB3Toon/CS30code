3ec48640e3eb7c01ae91d38555ffc6c72a5ca5a3
MacB3Toon <126741808+MacB3Toon@users.noreply.github.com>
2023-03-20 15:24:05 -0600

    2dgrid

---
+++ 7-2d-grid/sketch.js
@@ -0,0 +1,77 @@
+// 2d array demo -- drawing grid
+// Your Name
+// mar 20, 2023
+
+// let grid = [[0,0,1,1,] //y = 0
+//             [1,1,0,0] //y=1
+//             [0,1,0,1] //y=2
+//             [1,1,1,1]]; //y=3
+
+let grid;
+const ROWS = 10;
+const COLS = 10;
+let cellSize;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  grid = createRandomGrid(ROWS, COLS);
+
+  if (width < height){
+    cellSize = width/ROWS;
+  }
+  else{
+    cellSize = height/ROWS;
+  }
+}
+
+function draw() {
+  background(220);
+  displayGrid();
+}
+
+function keyTuped(){
+  if (key === "r"){
+    grid = createRandomGrid(ROWS, COLS);
+  }
+}
+function mousePressed(){
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  if (grid[y][x] === 0){
+    grid[y][x] = 1;
+  }
+  else if (grid[y][x] === 1){
+    grid[y][x] = 0;
+  }
+}
+
+function displayGrid(){
+  for (let y = 0; y< ROWS; y++){
+    for (let x = 0; x < COLS; x++){
+      if (grid[y][x] === 1){
+        fill("black");
+      }
+      else if (grid[y][x] === 0){
+        fill("white");
+      }
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function createRandomGrid(ROWS, COLS){
+  let newGrid = [];
+  for (let y = 0; y < ROWS; y++){
+    newGrid.push([]);
+    for (let x = 0; x < COLS; x++){
+      if (random(100) < 50){
+        newGrid[y].push(1);
+      }
+      else{
+        newGrid[y].push(0);
+      }
+    }
+  }
+  return newGrid;
+}
