f2ee16c10feb0430f8c6c4cf8d5c1a7807a0d7b7
MacB3Toon <126741808+MacB3Toon@users.noreply.github.com>
2023-03-24 15:19:47 -0600

    catgame

--- cat-game/sketch.js
+++ cat-game/sketch.js
@@ -3,12 +3,17 @@
 // Date
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - Uploaded an image and moved it based on where the user clicked
 
-const XROWS = 20;
+const XROWS = 21;
 const YCOLS = 35;
 let grid;
-let cellSize;
+let cellSize; //size of squares
+let img = createImage();
+
+function preload(){
+
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -21,11 +26,19 @@ function setup() {
   else {
     cellSize = height/XROWS;
   }
+
+  grid = restartGame(XROWS, YCOLS);
 }
 
 function draw() {
   background(220);
   displayGrid(grid);
+}
+
+function keyTyped(){
+  if (key === "r"){
+    grid = restartGame(XROWS, YCOLS);
+  }
 }
 
 function mousePressed() {
@@ -34,7 +47,6 @@ function mousePressed() {
 
   toggleCell(x, y);
 }
-
 
 function toggleCell(x, y) {
   //sanity check
@@ -68,6 +80,24 @@ function restartGame(XROWS, YCOLS) {
     newGrid.push([]);
     for (let x = 0; x < YCOLS; x++) {
       newGrid[y].push(0);
+    }
+  }
+  newGrid = createRandomBricks(XROWS, YCOLS);
+  return newGrid;
+}
+
+function createRandomBricks(XROWS, YCOLS) {
+  let newGrid = [];
+  
+  for (let y = 0; y < XROWS; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < YCOLS; x++) {
+      if (random(100)> 5){
+        newGrid[y].push(0);
+      }
+      else{
+        newGrid[y].push(1);
+      }
     }
   }
   return newGrid;
