5912bfd2eab3f88131231a73c846bb02f65862f3
MacB3Toon <126741808+MacB3Toon@users.noreply.github.com>
2023-04-16 19:57:00 -0600

    cat game

--- cat-game/sketch.js
+++ cat-game/sketch.js
@@ -1,20 +1,20 @@
 // Trap the cat!
 // Macayla Buckmaster
-// Date
+// 4/15/2023
 //
 // Extra for Experts:
 // - Uploaded an image and moved it based on where the user clicked
-
-//get cat to move to empty square randomly
-//set size of grid to be based on window width and height use 12.
-//make the cat change img when it moves
 
 const XROWS = 15;
 const YCOLS = 29;
 let grid;
 let cellSize; //size of squares
 let cat;
-let choosedirection = ["up", "down", "left", "right"];
+let topLeft = ["down", "right"];
+let topRight = ["down", "left"];
+let botLeft = ["up", "right"];
+let botRight = ["up", "left"];
+let directionChoices = ["up", "down", "right", "left"];
 let catX;
 let catY;
 let direction;
@@ -25,6 +25,7 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  loop();
 
   grid = restartGame(XROWS, YCOLS);
 
@@ -59,11 +60,12 @@ function setup() {
 function draw() {
   background(220);
   displayGame(grid);
-  direction = drawCat(catY, catX);
+  drawCat(catY, catX);
 }
 
 function keyTyped(){
   if (key === "r"){
+    loop();
     grid = restartGame(XROWS, YCOLS);
   }
 }
@@ -72,11 +74,58 @@ function mousePressed() {
   let x = Math.floor(mouseX/cellSize);
   let y = Math.floor(mouseY/cellSize);
 
+  //sets the possible directions for the cat to choose based on where the user clicked
+  if (mouseX < width/2 && mouseY < height/2){// top left corner
+    direction = random(topLeft);
+  }
+  else if (mouseX > width/2 && mouseY < height/2){// top right corner
+    direction = random(topRight);
+  }
+  else if (mouseX > width/2 && mouseY > height/2){// bottom right corner
+    direction = random(botRight);
+  }
+  else if (mouseX < width/2 && mouseY > height/2){// bottom leftcorner
+    direction = random(botLeft);
+  }
+
   toggleCell(x, y);
   moveCat(direction, catY, catX);
 }
 
+function intoWall(direction){
+  //if the first direction chosen by cat is filled with a wall, it chooses another direction
+  let newDirection = [...directionChoices];
+  newDirection -= direction;
+  let possibleDirection = random(newDirection);
+
+  //choosing another direction that isn't filled by a wall
+  if (possibleDirection === "up"){
+    if (grid[catY - 1][catX] === 0){
+      moveCat(possibleDirection, catY, catX);
+    }
+  }
+  else if (possibleDirection === "down"){
+    if (grid[catY - 1][catX] === 0){
+      moveCat(possibleDirection, catY, catX);
+    }
+  }
+  else if (possibleDirection === "left"){
+    if (grid[catY - 1][catX] === 0){
+      moveCat(possibleDirection, catY, catX);
+    }
+  }
+  else if (possibleDirection === "right"){
+    if (grid[catY - 1][catX] === 0){
+      moveCat(possibleDirection, catY, catX);
+    }
+  }
+  else{ //if there is nowhere to go
+    youWin();
+  }
+}
+
 function moveCat(direction, catY, catX){ 
+  //moves the image of the cat based on where the user clicked
   if (direction === "up"){
     //check if going into wall wall = 1
     if(grid[catY - 1][catX] === 0){
@@ -90,9 +139,7 @@ function moveCat(direction, catY, catX){
       grid[tempY][tempX] = 0;
     }
     else{//if cat is going to run into a wall
-      let newDirection = [...direction];
-      newDirection -= direction;
-      moveCat(newDirection, catY, catX);
+      intoWall(direction);
     }
   }
   else if (direction === "down"){
@@ -108,9 +155,7 @@ function moveCat(direction, catY, catX){
       grid[tempY][tempX] = 0;
     }
     else{//if cat is going to run into a wall
-      let newDirection = [...direction];
-      newDirection -= direction;
-      moveCat(newDirection, catY, catX);
+      intoWall(direction);
     }
   }
   else if (direction === "left"){
@@ -126,9 +171,7 @@ function moveCat(direction, catY, catX){
       grid[tempY][tempX] = 0;
     }
     else{//if cat is going to run into a wall
-      let newDirection = [...direction];
-      newDirection -= direction;
-      moveCat(newDirection, catY, catX);
+      intoWall(direction);
     }
   }
   else if (direction === "right"){
@@ -144,33 +187,46 @@ function moveCat(direction, catY, catX){
       grid[tempY][tempX] = 0;
     }
     else{//if cat is going to run into a wall
-      let newDirection = [...direction];
-      newDirection -= direction;
-      moveCat(newDirection, catY, catX);
+      intoWall(direction);
     }
+  }
+}
+
+function youWin(){
+  //if the cat gets trapped with nowhere left to go
+  if(grid[catY - 1][catX] === 1 && grid[catY][catX - 1] === 1 && grid[catY][catX + 1] === 1 &&  grid[catY + 1][catX] === 1){
+    noLoop();
+    rectMode(CENTER);
+    fill(153, 51, 255);
+    rect(windowWidth/2, windowHeight/2, 400, 200);
+    fill("black");
+    textAlign(CENTER);
+    textSize(40);
+    text("You Win!", windowWidth/2, windowHeight/2 - 50);
+    textSize(35);
+    text("You Trapped The Cat!", windowWidth/2, windowHeight/2);
+    textSize(20);
+    text("press R key to restart", windowWidth/2, windowHeight/2 + 50);
   }
 }
 
 function drawCat(catY, catX){
   image(cat, catX*cellSize  + cellSize/2, catY*cellSize  + cellSize/2, cellSize - 1, cellSize - 1);
-  direction = random(choosedirection);
-  return direction;
 }
 
 function toggleCell(x, y) {
-  //0 = floor, 1 = wall. 
+  //makes a floor space into a wall
   if (x >= 0 && x < YCOLS && y >= 0 && y < XROWS) {
     if (grid[y][x] === 0) { 
       grid[y][x] = 1;
-    }
-    else if (grid[y][x] === 1) {
-      grid[y][x] = 0;
     }
   }
 }
 
 function displayGame(grid) {
+  //creates the grid for the game to be played on 
   imageMode(CENTER);
+  rectMode(CORNER);
   for (let y = 0; y < XROWS; y++) {
     for (let x = 0; x < YCOLS; x++) {
       if (grid[y][x] === 0) {
@@ -191,6 +247,7 @@ function displayGame(grid) {
 }
 
 function restartGame(XROWS, YCOLS) {
+  //resets the cat in the middle, with random walls on the grid
   let newGrid = [];
   for (let y = 0; y < XROWS; y++) {
     newGrid.push([]);
@@ -203,6 +260,7 @@ function restartGame(XROWS, YCOLS) {
 }
 
 function createRandomBricks(XROWS, YCOLS) {
+  //makes the random walls appear in the grid
   let newGrid = [];
   
   for (let y = 0; y < XROWS; y++) {
